c254e50b7fc5f65b8739167c79a9808030561428
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-11-06 19:41:12 +0530

    new

--- gallery/settings.py
+++ gallery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lo-v+0%jl5r@v9pvwzywbd+frj47$0&#e%90i9ene!+=2wtjo0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
