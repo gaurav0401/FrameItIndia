992038005d9000f05a2e3095a9ad172e6dfeab0d
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-11-06 20:07:39 +0530

    new

--- gallery/settings.py
+++ gallery/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL="/media/"
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build' , 'static')
 # STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
