849336fecc5cdb7eb1cfa2efd86cda39ea770ce7
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-11-06 19:51:50 +0530

    new

--- gallery/settings.py
+++ gallery/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build' , 'static')
