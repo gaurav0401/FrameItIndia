33cf1393434fe0558b7bab0fcf3515219d6e312d
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-11-09 00:20:45 +0530

    new

--- home/models.py
+++ home/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Images(models.Model):
-    photo=models.ImageField()
+    photo=models.ImageField(upload_to="myImage/")
     date=models.DateTimeField(auto_now_add=True)
