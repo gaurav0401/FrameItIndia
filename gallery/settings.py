cb482e0d738cfc2e01cc18602b57fbb466b324ef
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-11-09 00:27:04 +0530

    new

--- gallery/settings.py
+++ gallery/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [  os.path.join(BASE_DIR, 'static')  ]
+
+
 MEDIA_URL="/media/myImage/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
